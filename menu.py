7448276ccb8b2864b2db71e85842ecb0363aaf15
Pelle J Nordentoft <150009779+PelleJN@users.noreply.github.com>
2024-02-23 14:57:05 +0100

    Partial v0.3.0 upload

--- menu.py
+++ menu.py
@@ -1,11 +1,14 @@
-#version 0.2.1
+#version 0.3.0
 
 import nuke
 
 nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./images')
+nuke.pluginAddPath('./python')
 
 print("Loading Visual Magic Toolkit")
+
+nuke.load('BKR_Main')
 
 #setup menu
 toolbar = nuke.toolbar('Nodes')
@@ -13,10 +16,12 @@ vmNode = toolbar.addMenu('Visual Magic', icon = 'vmlogo.png')
 
 #add gizmos
 vmNode.addCommand('AOVEdit', 'nuke.createNode(\"AOVEdit\")')
-vmNode.addCommand('Despeckle', 'nuke.createNode(\"Despeckle\")')
+vmNode.addCommand('Despeckle', 'nuke.createNode(\"Despeckle\")', icon="despeckle.png")
 vmNode.addCommand('DotGrid', 'nuke.createNode(\"DotGrid\")')
 vmNode.addCommand('NoiseBegone', 'nuke.createNode(\"Noise_Begone\")')
-vmNode.addCommand('NormalLight', 'nuke.createNode(\"NormalLight\")')
+vmNode.addCommand('NormalLight', 'nuke.createNode(\"NormalLight\")', icon="normallight.png")
+vmNode.addCommand('NukeBake', 'nuke.createNode("NukeBake") if nuke.script_directory() and nuke.script_directory() != "" else nuke.message("Please save your script before trying to use NukeBake!")', icon="nukebake.png") #Only add if script is saved.
 vmNode.addCommand('Toaster', 'nuke.createNode(\"Toaster\")')
 
 print("Visual Magic Toolkit Successfully Loaded")
+
